dc2c987f74043d677a7b0a3bbe486033a2f42a29
miyuki@Miya <kisenana@gmail.com>
2024-02-28 15:54:02 +0800

    Add button drawing method override

--- Source/CustomLookAndFeel.h
+++ Source/CustomLookAndFeel.h
@@ -20,6 +20,10 @@ public:
     // Override the drawRotarySlider method
     void drawRotarySlider(Graphics&, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, Slider&) override;
 
+    // Override the drawButtonBackground method
+	void drawButtonBackground(Graphics&, Button&, const Colour& backgroundColour,
+								bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override;
+
     // Custom Typeface
     juce::Typeface::Ptr smoochsans {juce::Typeface::createSystemTypefaceFor(BinaryData::SmoochSansRegular_ttf, BinaryData::SmoochSansRegular_ttfSize)};
     juce::Typeface::Ptr smoochsansMedium {juce::Typeface::createSystemTypefaceFor(BinaryData::SmoochSansMedium_ttf, BinaryData::SmoochSansMedium_ttfSize)};
